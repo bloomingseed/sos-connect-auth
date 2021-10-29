b69acdc607077849da06de387a06f428b01481da
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-10-29 20:48:22 +0700

    merged development

--- seeders/seeder-user.js
+++ seeders/seeder-user.js
@@ -1,16 +1,25 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
+    let seedings = [];
+    for (let i = 0; i < 20; ++i) {
+      seedings.push({
+        username: `seeding.user.${i + 1}`,
+        password_hash: "jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI=",
+        date_created: new Date(),
+      });
+    }
+    await queryInterface.bulkInsert("Accounts", seedings, {});
     return queryInterface.bulkInsert(
       "Accounts",
       [
         {
           username: "bloomingseed",
-          password_hash: "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=",
+          password_hash: "jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI=",
           date_created: new Date(),
         },
         {
           username: "nvcgooner",
-          password_hash: "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=",
+          password_hash: "jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI=",
           date_created: new Date(),
         },
       ],

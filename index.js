744fc6375527b913e0b3c121e61edec42e50ddee
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-11-30 22:24:57 +0700

    Enabled CORS for all routes and all methods

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const config = require("./config");
 const app = express();
 const port = config.PORT || 3000;
 const routers = require("./routers");
 
+app.use(cors());
 app.use(express.json());
 console.log(routers);
 routers.forEach((router) => app.use(`/${router.name}`, router.router));

9736533af0508f6977e75b8a24b5f11978b11baa
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-12-09 22:40:22 +0700

    Fixed some status code, add  more request body, updated docs

--- helpers/index.js
+++ helpers/index.js
@@ -8,7 +8,7 @@ function getAuthToken(req) {
 function authUserMiddleware(req, res, next) {
   let accessToken = getAuthToken(req);
   if (accessToken == null) {
-    return res.status(401).json({
+    return res.status(403).json({
       error:
         'Request header "Authentication" does not exist or does not contain authentication token.',
     });
@@ -16,7 +16,7 @@ function authUserMiddleware(req, res, next) {
   try {
     req.verifyResult = jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET);
   } catch (err) {
-    return res.status(401).json({ error: "Access token is invalid" });
+    return res.status(403).json({ error: "Access token is invalid" });
   }
   next();
 }
